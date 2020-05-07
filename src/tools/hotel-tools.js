5f0d311d6295b88b06a92beadd8c369c1949e783
Anh Tân <anhtan3396@gmail.com>
2020-05-07 15:39:08 +0700

    view log

--- src/tools/hotel-tools.js
+++ src/tools/hotel-tools.js
@@ -8,7 +8,7 @@ async function processHotel(placeInfo, client, dataset) {
     const { location_id: id } = placeInfo;
     let reviews = [];
     let placePrices;
-    log.info(placeInfo);
+    log.info(placeInfo.photo);
     
     try {
       //  placePrices = await getPlacePrices(id, randomDelay);
