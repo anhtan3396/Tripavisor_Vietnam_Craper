fcf59e80af0cbf6ab6816a0bd07383a750cf0fa5
Anh Tân <anhtan3396@gmail.com>
2020-05-15 15:05:20 +0700

    fix curency

--- src/tools/api.js
+++ src/tools/api.js
@@ -66,7 +66,7 @@ async function getLocationId(searchString, session) {
 
 async function getPlacePrices(placeId, delay, session) {
     const dateString = global.CHECKIN_DATE || moment().format('YYYY-MM-DD');
-    const url = `https://api.tripadvisor.com/api/internal/1.19/vi/meta_hac/${placeId}?adults=2&checkin=${dateString}&currency=VND&lod=extended&nights=1&lang=${global.LANGUAGE}`;
+    const url = `https://api.tripadvisor.com/api/internal/1.19/en/meta_hac/${placeId}?adults=2&checkin=${dateString}&currency=VND&lod=extended&nights=1&lang=${global.LANGUAGE}`;
     const response = await axios.get(
         url,
         {
@@ -138,7 +138,7 @@ async function callForAttractionReview(locationId, session, limit = 10, offset =
 }
 
 async function getReviewTagsForLocation(locationId, session, limit = LIMIT, offset = 0) {
-    const url = `https://api.tripadvisor.com/api/internal/1.14/location/${locationId}/keywords?currency=VND&lang=${global.LANGUAGE}&limit=${limit}&offset=${offset}`;
+    const url = `https://api.tripadvisor.com/api/internal/1.14/location/${locationId}/keywords?currency=CZK&lang=${global.LANGUAGE}&limit=${limit}&offset=${offset}`;
     const response = await axios.get(
         url,
         { headers: { 'X-TripAdvisor-API-Key': API_KEY, Cookie: session.getCookieString(url) }, ...getAgentOptions(session) },
@@ -149,7 +149,7 @@ async function getReviewTagsForLocation(locationId, session, limit = LIMIT, offs
 }
 
 async function callForRestaurantList(locationId, session, limit = LIMIT, offset = 0) {
-    const url = `https://api.tripadvisor.com/api/internal/1.14/location/${locationId}/restaurants?currency=VND&lang=${global.LANGUAGE}&limit=${limit}&offset=${offset}`;
+    const url = `https://api.tripadvisor.com/api/internal/1.14/location/${locationId}/restaurants?currency=CZK&lang=${global.LANGUAGE}&limit=${limit}&offset=${offset}`;
     const response = await axios.get(
         url,
         { headers: { 'X-TripAdvisor-API-Key': API_KEY, Cookie: session.getCookieString(url) }, ...getAgentOptions(session) },
@@ -160,7 +160,7 @@ async function callForRestaurantList(locationId, session, limit = LIMIT, offset 
 }
 
 async function callForHotelList(locationId, session, limit = LIMIT, offset = 0) {
-    const url = `https://api.tripadvisor.com/api/internal/1.14/location/${locationId}/hotels?currency=VND&lang=${global.LANGUAGE}&limit=${limit}&offset=${offset}`;
+    const url = `https://api.tripadvisor.com/api/internal/1.14/location/${locationId}/hotels?currency=CZK&lang=${global.LANGUAGE}&limit=${limit}&offset=${offset}`;
     const response = await axios.get(
         url,
         { headers: { 'X-TripAdvisor-API-Key': API_KEY, Cookie: session.getCookieString(url) }, ...getAgentOptions(session) },
